d3730982fec367e4e5acca8de79a29abfee35a77
Jonas Henriksen <56156281+JonasHenriksen123@users.noreply.github.com>
2026-02-01 16:25:41 +0100

    Move artifact registry to region euwest1

--- deploy.ps1
+++ deploy.ps1
@@ -2,9 +2,9 @@
 # Build and push application docker image to Google cloud platform
 
 Write-Host "Building docker image..."
-& docker build . --tag gcr.io/sukkeregern-stregliste-277311/streglisteangularnode:latest
+& docker build . --tag eu.gcr.io/sukkeregern-stregliste-277311/streglisteangularnode:latest
 Write-Host "Build done"
 
 Write-Host "Pushing docker image to repository..."
-& docker push gcr.io/sukkeregern-stregliste-277311/streglisteangularnode:latest
+& docker push eu.gcr.io/sukkeregern-stregliste-277311/streglisteangularnode:latest
 Write-Host "Done"
