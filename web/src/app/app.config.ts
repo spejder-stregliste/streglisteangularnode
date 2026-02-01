c937caf1882fa53558436bab4513f228edc02e42
Jonas Henriksen <56156281+JonasHenriksen123@users.noreply.github.com>
2026-02-01 16:14:14 +0100

    Enable lazy loading of components

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -6,18 +6,15 @@ import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatSelectModule } from '@angular/material/select';
-import { provideAnimations } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
 import { MatSortModule } from '@angular/material/sort';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withPreloading, PreloadAllModules } from '@angular/router';
 
 import { UserService } from './services/user.service';
 import { GlobalService } from './services/global.service';
 import { AdminService } from './services/admin.service';
 import { HomeComponent } from './home/home.component';
-import { QRComponent } from './qr/qr.component';
-import { AdminComponent } from './admin/admin.component';
 
 export function getBaseUrl() {
     return document.getElementsByTagName('base')[0].href;
@@ -36,15 +33,28 @@ const appConfig: ApplicationConfig =
         AdminService,
         provideHttpClient(withInterceptorsFromDi(), withFetch()),
         { provide: APP_ID, useValue: 'ng-cli-universal' },
-        provideAnimations(),
-        provideRouter([
-            { path: '', component: HomeComponent, pathMatch: 'full' },
-            { path: 'qr', component: QRComponent, pathMatch: 'full' },
-            { path: 'admin', component: AdminComponent, pathMatch: 'full' }
-        ]),
+        provideRouter(
+            [
+                {
+                    path: '',
+                    component: HomeComponent,
+                    pathMatch: 'full',
+                },
+                {
+                    path: 'qr',
+                    loadComponent: () => import('./qr/qr.component').then((m) => m.QRComponent),
+                    pathMatch: 'full'
+                },
+                {
+                    path: 'admin',
+                    loadComponent: () => import('./admin/admin.component').then((m) => m.AdminComponent),
+                    pathMatch: 'full'
+                }
+            ],
+            withPreloading(PreloadAllModules)
+        ),
         ...providers, provideClientHydration(withEventReplay())
     ]
-
 };
 
 export default appConfig;
--- web/src/app/home/home.component.ts
+++ web/src/app/home/home.component.ts
@@ -1,128 +1,19 @@
-import { Component, effect, OnDestroy, OnInit, signal, WritableSignal } from '@angular/core';
-import { Subscription } from 'rxjs';
-import { User, UserService } from '../services/user.service';
-import { MatDialog } from '@angular/material/dialog';
-import { DialogComponent } from '../dialog/dialog.component';
-import { GlobalService, Status } from '../services/global.service';
-import { Sort, MatSort, MatSortHeader } from '@angular/material/sort';
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { NgStyle } from '@angular/common';
-import { MatTable, MatColumnDef, MatHeaderCellDef, MatHeaderCell, MatCellDef, MatCell, MatHeaderRowDef, MatHeaderRow, MatRowDef, MatRow, MatTableDataSource } from '@angular/material/table';
-import { MatIcon } from '@angular/material/icon';
-import { MatInput } from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
-import { MatMiniFabButton, MatButton } from '@angular/material/button';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { StreglisteComponent } from "../stregliste/stregliste.component";
+import { UserService } from '../services/user.service';
+import { GlobalService } from '../services/global.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css'],
-  imports: [RouterLink, MatTable, MatSort, MatColumnDef, MatHeaderCellDef, MatHeaderCell, MatSortHeader, MatIcon, MatCellDef, MatCell, MatInput, FormsModule, NgStyle, MatMiniFabButton, MatButton, MatHeaderRowDef, MatHeaderRow, MatRowDef, MatRow, MatFormField, MatLabel]
+  imports: [RouterLink, FormsModule, StreglisteComponent]
 })
 export class HomeComponent {
-  readonly displayedColumns: string[] = ['name', 'lines', 'amount', 'edit'];
-  readonly datasource: MatTableDataSource<User> = new MatTableDataSource();
-  readonly users: WritableSignal<User[]>;
-  readonly status: WritableSignal<Status> = signal({ status: "loading" });
-  readonly creating: WritableSignal<boolean> = signal(false);
-  readonly editingRows: WritableSignal<Set<string>> = signal(new Set())
-  newUser: string = "";
-  private sort: WritableSignal<Sort | undefined> = signal(undefined);
-
-  constructor(private userService: UserService, public dialog: MatDialog, private globalService: GlobalService) {
-    this.users = userService.users;
-    effect(() => this.users.update(users => this.sortData(users, this.sort())));
-    this.status = this.globalService.status
-    effect(() => this.datasource.data = this.users());
+  constructor(private _: UserService, private __: GlobalService) {
+    // intentionally empty
+    // Create the user- and globalService to force preload of data for stregliste
   }
-
-  setEditing(user: User, editing: boolean) {
-    this.editingRows.update(s => {
-      const set = new Set(s)
-      editing ? set.add(user.name!) : set.delete(user.name!);
-      return set
-    })
-  }
-
-  update(user: User) {
-    this.userService.updateUser(user).then(res => {
-      if (res.status) {
-        this.setEditing(user, false);
-        if (res?.lines !== undefined && res?.lines >= 40) {
-          this.dialog.open(DialogComponent, {
-            data: { name: user.name },
-          });
-        }
-      }
-      else {
-        window.alert("Kan ikke gemme");
-      }
-    }, () => {
-      window.alert("Kan ikke gemme");
-      this.userService.updateAll().then(() => {
-      }, () => {
-        this.users.set([]);
-      });
-    })
-  }
-
-  increment(user: User) {
-    this.userService.increment(user)
-  }
-
-  decrement(user: User) {
-    this.userService.decrement(user)
-  }
-
-  createUser() {
-    this.creating.set(true);
-  }
-
-  sendNewUser() {
-    if (this.newUser == null || this.newUser.length < 1) {
-      return;
-    }
-    this.creating.set(false);
-    this.userService.addUser(new User(this.newUser, 0)).then(res => {
-      if (!res) {
-        window.Error("Kan ikke oprette bruger");
-        this.users.set([]);
-      }
-    }, () => {
-      window.Error("Kan ikke oprette bruger");
-      this.users.set([]);
-    })
-    this.newUser = ""
-  }
-
-  sortData(users: User[], sort: Sort | undefined) {
-    const data = [...users];
-    if (!sort || !sort.active || sort.direction === '') {
-      return data;
-    }
-
-    return data.sort((a, b) => {
-      const isAsc = sort!.direction === 'asc';
-      switch (sort!.active) {
-        case 'name':
-          return compare({ first: a.name ?? "", second: a.lines ?? 0 }, { first: b.name ?? "", second: b.lines ?? 0 }, isAsc);
-        case 'lines':
-          return compare({ first: a.lines ?? 0, second: a.name ?? "" }, { first: b.lines ?? 0, second: b.name ?? "" }, isAsc);
-        default:
-          return 0;
-      }
-    });
-  }
-
-  setSort(sort: Sort) {
-    this.sort.set(sort);
-  }
-}
-
-function compare(a: { first: number | string, second: number | string }, b: { first: number | string, second: number | string }, isAsc: boolean) {
-  if (a.first === b.first) {
-    return (a.second < b.second ? -1 : 1);
-  }
-  return (a.first < b.first ? -1 : 1) * (isAsc ? 1 : -1);
 }
